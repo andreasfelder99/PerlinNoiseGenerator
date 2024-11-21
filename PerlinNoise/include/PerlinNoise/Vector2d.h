c3684a596e9730f5f09b408075027688dd4bc0b1
Andreas Felder <andreas.felder@students.fhnw.ch>
2024-11-21 12:47:27 +0100

    move implementation of Vector2d from header to cpp file

--- PerlinNoise/include/PerlinNoise/Vector2d.h
+++ PerlinNoise/include/PerlinNoise/Vector2d.h
@@ -1,116 +1,51 @@
-    #pragma once
-    #include <cmath>
-    #include <utility>
+#pragma once
 
-    #ifndef VECTOR2D_H
-    #define VECTOR2D_H
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
 
-    class Vector2d {
-    public:
-        Vector2d() : m_x(0.0), m_y(0.0) {
-        }
+class Vector2d {
+public:
+    Vector2d();
 
-        Vector2d(double x, double y) : m_x(x), m_y(y) {
-        }
+    Vector2d(double x, double y);
 
-        //Copy constructor is default, but we explicitly mention it because of the rule of 5
-        Vector2d(const Vector2d &other) = default;
+    Vector2d(const Vector2d &other) = default; // Copy constructor
+    Vector2d(Vector2d &&other) noexcept; // Move constructor
+    ~Vector2d() = default; // Destructor
 
-        //Move constructor
-        Vector2d(Vector2d &&other) noexcept : m_x(std::exchange(other.m_x, 0)), m_y(std::exchange(other.m_y, 0)) {
-        }
+    Vector2d &operator=(Vector2d &&other) noexcept; // Move assignment
+    Vector2d &operator=(const Vector2d &other); // Copy assignment
 
-        //Destructor is default, but we explicitly mention it because of the rule of 5
-        ~Vector2d() = default;
+    // Getters
+    [[nodiscard]] double x() const;
 
-        //Move assignment
-        Vector2d &operator=(Vector2d &&other) noexcept {
-            if (this == &other) {
-                return *this;
-            }
+    [[nodiscard]] double y() const;
 
-            std::swap(m_x, other.m_x);
-            std::swap(m_y, other.m_y);
+    // Arithmetic operations
+    double operator*(const Vector2d &other) const;
 
-            return *this;
-        }
+    Vector2d operator*(double c) const;
 
-        //Copy assignment
-        Vector2d &operator=(const Vector2d &other) {
-            if (this == &other) {
-                return *this;
-            }
+    friend Vector2d operator*(double c, const Vector2d &vec);
 
-            m_x = other.m_x;
-            m_y = other.m_y;
+    Vector2d operator/(double c) const;
 
-            return *this;
-        }
+    Vector2d operator+(const Vector2d &other) const;
 
-        // getters
-        [[nodiscard]] double x() const {
-            return m_x;
-        }
+    Vector2d operator-(const Vector2d &other) const;
 
-        [[nodiscard]] double y() const {
-            return m_y;
-        }
+    // Normalization functions
+    [[nodiscard]] Vector2d normalized() const;
 
-        // arithmetic operations
-        double operator*(const Vector2d &other) const {
-            return m_x * other.x() + m_y * other.y();
-        }
+    void normalize();
 
-        Vector2d operator*(const double c) const {
-            return {m_x * c, m_y * c};
-        }
+    [[nodiscard]] double norm() const;
 
-        friend Vector2d operator*(const double c, const Vector2d &vec) {
-            return {vec.m_x * c, vec.m_y * c};
-        }
+private:
+    double m_x;
+    double m_y;
 
-        Vector2d operator/(const double c) const {
-            return {m_x / c, m_y / c};
-        }
+    [[nodiscard]] double magnitude() const;
+};
 
-        Vector2d operator+(const Vector2d &other) const {
-            return {m_x + other.x(), m_y + other.y()};
-        }
-
-        Vector2d operator-(const Vector2d &other) const {
-            return {m_x - other.x(), m_y - other.y()};
-        }
-
-        // normalization functions
-        [[nodiscard]] Vector2d normalized() const {
-            if (magnitude() == 0.0) {
-                // If the magnitude is 0, return a zero vector to avoid division by zero
-                return {0.0, 0.0};
-            }
-            return {m_x / magnitude(), m_y / magnitude()};
-        }
-
-        void normalize() {
-            //If we have the same problem as above, we just set it to 0
-            if (magnitude() == 0.0) {
-                m_x = 0;
-                m_y = 0;
-            }
-            m_x = m_x / magnitude();
-            m_y = m_y / magnitude();
-        }
-
-        [[nodiscard]] double norm() const {
-            return magnitude();
-        }
-
-    private:
-        double m_x;
-        double m_y;
-
-        [[nodiscard]] double magnitude() const {
-            return std::sqrt(m_x * m_x + m_y * m_y);
-        }
-    };
-
-    #endif
+#endif
--- PerlinNoise/src/Vector2d.cpp
+++ PerlinNoise/src/Vector2d.cpp
@@ -1,4 +1,93 @@
-#include <cassert>
-#include <cmath>
-
 #include "Vector2d.h"
+#include <utility>
+
+// Constructors
+Vector2d::Vector2d() : m_x(0.0), m_y(0.0) {
+}
+
+Vector2d::Vector2d(double x, double y) : m_x(x), m_y(y) {
+}
+
+Vector2d::Vector2d(Vector2d &&other) noexcept
+    : m_x(std::exchange(other.m_x, 0)), m_y(std::exchange(other.m_y, 0)) {
+}
+
+// Move assignment operator
+Vector2d &Vector2d::operator=(Vector2d &&other) noexcept {
+    if (this == &other) {
+        return *this;
+    }
+    std::swap(m_x, other.m_x);
+    std::swap(m_y, other.m_y);
+    return *this;
+}
+
+// Copy assignment operator
+Vector2d &Vector2d::operator=(const Vector2d &other) {
+    if (this == &other) {
+        return *this;
+    }
+    m_x = other.m_x;
+    m_y = other.m_y;
+    return *this;
+}
+
+// Getters
+double Vector2d::x() const {
+    return m_x;
+}
+
+double Vector2d::y() const {
+    return m_y;
+}
+
+// Arithmetic operations
+double Vector2d::operator*(const Vector2d &other) const {
+    return m_x * other.x() + m_y * other.y();
+}
+
+Vector2d Vector2d::operator*(const double c) const {
+    return {m_x * c, m_y * c};
+}
+
+Vector2d operator*(const double c, const Vector2d &vec) {
+    return {vec.m_x * c, vec.m_y * c};
+}
+
+Vector2d Vector2d::operator/(const double c) const {
+    return {m_x / c, m_y / c};
+}
+
+Vector2d Vector2d::operator+(const Vector2d &other) const {
+    return {m_x + other.x(), m_y + other.y()};
+}
+
+Vector2d Vector2d::operator-(const Vector2d &other) const {
+    return {m_x - other.x(), m_y - other.y()};
+}
+
+// Normalization functions
+Vector2d Vector2d::normalized() const {
+    if (magnitude() == 0.0) {
+        return {0.0, 0.0};
+    }
+    return {m_x / magnitude(), m_y / magnitude()};
+}
+
+void Vector2d::normalize() {
+    if (magnitude() == 0.0) {
+        m_x = 0;
+        m_y = 0;
+    } else {
+        m_x = m_x / magnitude();
+        m_y = m_y / magnitude();
+    }
+}
+
+double Vector2d::norm() const {
+    return magnitude();
+}
+
+double Vector2d::magnitude() const {
+    return std::sqrt(m_x * m_x + m_y * m_y);
+}
